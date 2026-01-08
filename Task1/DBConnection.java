5fd5dc90d6ce1ceee7f702584c8cfafc7d85b0d8
harshbhaturkar1404 <harshbhaturkar14@gmail.com>
2026-01-08 23:26:37 +0530

    Add files via upload

--- Task1/DBConnection.java
+++ Task1/DBConnection.java
@@ -1,1 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class DBConnection {
+    public static void main(String[] args) {
 
+        String mysqlDriver = "com.mysql.cj.jdbc.Driver";//DataBase Connection Detail
+        String mysqlUrl = "jdbc:mysql://localhost:3306/JdCon";
+        String userName = "root";
+        String password = "harsh";
+        try {
+
+            Class.forName(mysqlDriver);
+
+            Connection con = DriverManager.getConnection(mysqlUrl, userName, password);
+            //Connection with database
+            System.out.println("CONNECTION SUCCESSFUL...");
+
+            con.close();
+        } catch (Exception ex) {
+            System.out.println("CONNECTION FAILED!");
+            ex.printStackTrace();
+        }
+    }
+}
